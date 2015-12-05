bf4e71858d920964e4ab0c7536d1298d255c3253
Russell Gordon <rgordon@rsgc.on.ca>
2015-12-05 07:03:30 -0500

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Russell Gordon on 2015-12-05.
+//  Copyright © 2015 Royal St. George's College. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
