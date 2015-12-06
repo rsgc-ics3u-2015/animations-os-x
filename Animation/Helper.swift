d200f3d72dd660264534abab37a1ad82e09ad3cb
Russell Gordon <rgordon@rsgc.on.ca>
2015-12-06 11:13:52 -0500

    Improved error checking of values set on instances of Color class.

--- Animation/Helper.swift
+++ Animation/Helper.swift
@@ -3,75 +3,119 @@ import Foundation
 
 public class Color {
     
+    // FIXME: Need more research into how to properly write a class that handles invalid property geting/setting
+    //
+    // If I want to write a class that "fixes" or rationalizes invalid values provided to it, when:
+    //
+    // 1. initializing a new object
+    // 2. setting properties of the object
+    //
+    // ...what is the correct way to do this in Swift?
+    //
+    // The purpose of the Color class I have written is to take hue, saturation, brightness,
+    // and alpha values range between 0-360, 0-100, 0-100, and 0-100 respectively.
+    //
+    // My thought was that property observers are the answer:
+    //
+    // https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Properties.html
+    //
+    // but Apple's documentation explicitly states that property observers are not called when
+    // a property is set from within an initializer:
+    //
+    // https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Initialization.html#//apple_ref/doc/uid/TP40014097-CH18-ID204
+    //
+    // NOTE
+    //
+    //  When you assign a default value to a stored property, or set its initial value within an initializer, the value of that property is set directly, without calling any property observers.
+    //
+    // As you can see, I am using private functions that are called from the initializer
+    // and the property observer (so I am not duplicating logic to rationalize the passed values).
+    //
+    // However, this seems inelegant.
+    //
+    // All in all, after doing more reading about failable initializers:
+    //
+    // https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Initialization.html#//apple_ref/doc/uid/TP40014097-CH18-ID203
+    //
+    // ... it really seems like perhaps I should NOT be trying to rationalize invalid values. Perhaps, instead, I should just fail to initialize an object if bad values are provided.
+    //
+    // Future self – something to think about.
+    
     var hue: Float = 0.0 {
         didSet {
-            if (hue < 0) {
-                hue = 0
-            }
-            if (hue > 360) {
-                hue = hue % 360
-            }
+            hue = self.rationalizeToSinglePositiveRotation(hue)
+            self.translatedHue = CGFloat(self.hue / 360)
         }
     }
     
     var saturation: Float = 0.0 {
         didSet {
-            if (saturation < 0) {
-                saturation = 0
-            }
-            if (saturation > 100) {
-                saturation = saturation % 100
-            }
+            saturation = self.rationalizePercentage(saturation)
+            self.translatedSaturation = CGFloat(self.saturation / 100)
         }
     }
     
-    var brightness: Float {
+    var brightness: Float = 0.0 {
         didSet {
-            if (brightness < 0) {
-                brightness = 0
-            }
-            if (brightness > 100) {
-                brightness = brightness % 100
-            }
+            brightness = self.rationalizePercentage(brightness)
+            self.translatedBrightness = CGFloat(self.brightness / 100)
         }
     }
     
-    var alpha: Float {
+    var alpha: Float = 0.0 {
         didSet {
-            if (alpha < 0) {
-                alpha = 0
-            }
-            if (alpha > 100) {
-                alpha = alpha % 100
-            }
+            alpha = self.rationalizePercentage(alpha)
+            self.translatedAlpha = CGFloat(self.alpha / 100)
         }
     }
     
-    var translatedHue: CGFloat
-    var translatedSaturation: CGFloat
-    var translatedBrightness: CGFloat
-    var translatedAlpha: CGFloat
-    
+    var translatedHue : CGFloat = 0.0
+    var translatedSaturation : CGFloat = 0.0
+    var translatedBrightness : CGFloat = 0.0
+    var translatedAlpha : CGFloat = 0.0
     
     public init(hue: Float, saturation: Float, brightness: Float, alpha: Float) {
         
-        // Set with provided values
-        if (hue > 360) {
-            self.hue = hue % 360
-        } else {
-            self.hue = hue
-        }
-        self.saturation = saturation
-        self.brightness = brightness
-        self.alpha = alpha
-        
+        // Set with provided values, but translate to valid values first
+        self.hue = rationalizeToSinglePositiveRotation(hue)
+        self.saturation = rationalizePercentage(saturation)
+        self.brightness = rationalizePercentage(brightness)
+        self.alpha = rationalizePercentage(alpha)
+    
         // Prepare values to provide to NSColor initializer
         self.translatedHue = CGFloat(self.hue / 360)
         self.translatedSaturation = CGFloat(self.saturation / 100)
         self.translatedBrightness = CGFloat(self.brightness / 100)
         self.translatedAlpha = CGFloat(self.alpha / 100)
+
+    }
+    
+    // Takes a given number of degrees and translates to range between 0 and 360
+    private func rationalizeToSinglePositiveRotation(value : Float) -> Float {
+        
+        if value < 0 {
+            return 0.0
+        } else if value > 360 {
+            return value % 360
+        }
+        
+        return value
         
     }
+    
+    // Takes a given value and translates to a percentage between 0 and 100
+    private func rationalizePercentage(value : Float) -> Float {
+        
+        if value < 0 {
+            return 0.0
+        } else if value > 100 {
+            return value % 100
+        }
+        
+        return value
+        
+    }
+    
 }
 
 @objc public class Canvas : NSObject {
@@ -144,6 +188,7 @@ public class Color {
         
         // Draw on the image that (presumably) has been defined on this canvas
         if let _ = self.canvas.image?.lockFocus() {
+            
             
             // Make the new path
             let path = NSBezierPath()
