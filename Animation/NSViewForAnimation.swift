3d59ae93504da83fb1e004121b49b862743b2714
Russell Gordon <rgordon@rsgc.on.ca>
2015-12-19 08:01:59 -0500

    Created sub-class of NSView; simplifies things on the storyboard a bit.
    Subclass of NSView may be useful for optimizing performance in future.

---
+++ Animation/NSViewForAnimation.swift
@@ -0,0 +1,19 @@
+//
+//  NSViewForAnimation.swift
+//  Animation
+//
+//  Created by Russell Gordon on 2015-12-19.
+//  Copyright © 2015 Royal St. George's College. All rights reserved.
+//
+
+import Cocoa
+
+class NSViewForAnimation: NSView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+                    
+}
--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -13,31 +13,20 @@ class ViewController: NSViewController {
     var timer = NSTimer()
     var sketch = Sketch()
     
-    @IBOutlet var myView: NSView!
+    @IBOutlet var myView: NSViewForAnimation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Tell OS that we want a layer to display an image on
         self.view.wantsLayer = true
+                
+        // Change size of the view to match canvas size
+        self.view.setFrameSize(NSSize(width: sketch.canvas.width, height: sketch.canvas.height))
         
         // Initialize the timer used to drive the sketch
         timer = NSTimer.scheduledTimerWithTimeInterval(1/Double(sketch.canvas.framesPerSecond), target: self, selector: Selector("timedDraw"), userInfo: nil, repeats: true)
         
-    }
-
-    override func viewWillAppear() {
-        super.viewWillAppear()
-        
-        // Before the view appears, resize to match the size of the canvas we are painting on
-        // See: http://stackoverflow.com/questions/27578085/resizing-window-to-view-controller-size-in-storyboard
-        preferredContentSize = NSSize(width: sketch.canvas.width, height: sketch.canvas.height)
-    }
-    
-    override var representedObject: AnyObject? {
-        didSet {
-            // Update the view, if already loaded.
-        }
     }
     
     func timedDraw() {
@@ -53,6 +42,7 @@ class ViewController: NSViewController {
         // view controller
         var imageRect : NSRect = NSMakeRect(0, 0, CGFloat(sketch.canvas.width), CGFloat(sketch.canvas.height))
         self.view.layer!.contents = sketch.canvas.imageView.image?.CGImageForProposedRect(&imageRect, context: NSGraphicsContext.currentContext(), hints: nil)
+
         
         
     }
