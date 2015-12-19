e55ea256ef7a91aac9dc46113992db9d71dd062e
Russell Gordon <rgordon@rsgc.on.ca>
2015-12-19 06:05:08 -0500

    A cloud of 100 balls move around the screen. Playing around with improving graphics performance.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -15,41 +15,92 @@ class Sketch {
     let canvas : Canvas
     
     // Declare any properties you need for your sketch below this comment, but before init()
-    var x = 0
-    var s = 1
+    var balls:[Ball] = [] // empty array of balls
 
     // This runs once, equivalent to setup() in Processing
     init() {
         
         // Create canvas object – specify size
-        canvas = Canvas(width: 500, height: 300)
+        canvas = Canvas(width: 900, height: 700)
         
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
+        
+        // create 100 bouncing balls
+        for i in 0...99 {
+            balls.insert(Ball(boundingWidth: canvas.width, boundingHeight: canvas.height), atIndex: i)
+        }
+        
+        // No borders for these shapes
+        canvas.drawShapesWithBorders = false
         
     }
     
     // Runs repeatedly, equivalent to draw() in Processing
     func draw() {
-                
-        // Horizontal position of circle
-        x = x + s
-        
-        // Bounce when hitting wall
-        if (x > canvas.width || x < 0) {
-            s *= -1
-        }
         
         // "Clear" the background with a semi-transparent black rectangle
-        canvas.drawShapesWithBorders = false
         canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 10)
         canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: canvas.height)
         
-        // Draw a circle that moves across the screen
-        canvas.drawShapesWithBorders = false
+        // Update and draw each ball
         canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: x, centreY: canvas.height / 2, width: 25, height: 25)
-                
+        for ball in balls {
+            
+            ball.updatePosition(on: canvas)
+            
+        }
+        
+    }
+    
+}
+
+// A bouncing ball
+class Ball {
+    var x : Int
+    var y : Int
+    var xSpeed : Int
+    var ySpeed : Int
+    
+    // intialize a ball within boundaries given
+    init(boundingWidth width: Int, boundingHeight height: Int) {
+        
+        // Random starting position somewhere on the cavas
+        self.x = Int(arc4random_uniform(UInt32(width)))
+        self.y = Int(arc4random_uniform(UInt32(height)))
+        
+        // Random speed of -1, -2, -3, 1, 2, 3, but not zero (all particles need to move)
+        self.xSpeed = Int(arc4random_uniform(UInt32(6))) - 3
+        self.ySpeed = Int(arc4random_uniform(UInt32(6))) - 3
+        if self.xSpeed == 0 {
+            self.xSpeed = 1
+        }
+        if self.ySpeed == 0 {
+            self.ySpeed = 1
+        }
+        
+    }
+    
+    // update the ball's position and then draw it
+    func updatePosition(on canvas: Canvas) {
+        
+        // Change position
+        x += xSpeed
+        y += ySpeed
+        
+        // Bounce on left and right sides
+        if x > canvas.width || x < 0 {
+            xSpeed *= -1
+        }
+        
+        // Bounce on top and bottom sides
+        if y > canvas.height || y < 0 {
+            ySpeed *= -1
+        }
+        
+        // Draw the ball in it's new position
+        canvas.drawEllipse(centreX: x, centreY: y, width: 5, height: 5)
+        
     }
     
 }
