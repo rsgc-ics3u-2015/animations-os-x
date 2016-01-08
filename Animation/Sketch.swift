372011bf9ca6c23bcb55796c0981a634ac2dd590
Russell Gordon <rgordon@rsgc.on.ca>
2016-01-08 17:08:38 -0500

    Now reads data from serial port and prints it to the console.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,24 +8,61 @@
 
 import Foundation
 
-class Sketch {
+// NOTE: The Sketch class will define the methods required by the ORSSerialPortDelegate protocol
+//
+// “A protocol defines a blueprint of methods, properties, and other requirements that suit a 
+// particular task or piece of functionality.”
+//
+// Excerpt From: Apple Inc. “The Swift Programming Language (Swift 2).” iBooks. https://itun.es/ca/jEUH0.l
+//
+// In this case, the Sketch class implements methods that allow us to read and use the serial port, via
+// the ORSSerialPort library.
+class Sketch : NSObject, ORSSerialPortDelegate {
 
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
     // Declare any properties you need for your sketch below this comment, but before init()
+    var serialPort : ORSSerialPort?       // Object required to read serial port
     var x = 0
     var s = 1
 
     // This runs once, equivalent to setup() in Processing
-    init() {
+    override init() {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 300)
         
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
+
+        // Call superclass initializer
+        super.init()
+        
+        // Find and list available ports
+        var availablePorts = ORSSerialPortManager.sharedSerialPortManager().availablePorts
+        if availablePorts.count == 0 {
+            
+            // Show error message if no ports found
+            print("No connected serial ports found. Please connect your USB to serial adapter(s) and run the program again.\n")
+            exit(EXIT_SUCCESS)
+            
+        } else {
+            
+            // List available ports in debug window (view this and adjust
+            print("Available ports are...")
+            for (i, port) in availablePorts.enumerate() {
+                print("\(i). \(port.name)")
+            }
+            
+            // Open the desired port
+            serialPort = availablePorts[0]  // selecting first item in list of available ports
+            serialPort!.baudRate = 9600
+            serialPort!.delegate = self
+            serialPort!.open()
+            
+        }
         
     }
     
@@ -50,6 +87,30 @@ class Sketch {
         canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
         canvas.drawEllipse(centreX: x, centreY: canvas.height / 2, width: 25, height: 25)
                 
+    }
+    
+    // ORSSerialPortDelegate
+    // These four methods are required to conform to the ORSSerialPort protocol
+    // (Basically, the methods will be invoked when serial port events happen)
+    func serialPort(serialPort: ORSSerialPort, didReceiveData data: NSData) {
+        
+        // Print whatever we receive off the serial port to the console
+        if let string = NSString(data: data, encoding: NSUTF8StringEncoding) {
+            print("\(string)", terminator: "")
+        }
+        
+    }
+    
+    func serialPortWasRemovedFromSystem(serialPort: ORSSerialPort) {
+        self.serialPort = nil
+    }
+    
+    func serialPort(serialPort: ORSSerialPort, didEncounterError error: NSError) {
+        print("Serial port (\(serialPort)) encountered error: \(error)")
+    }
+    
+    func serialPortWasOpened(serialPort: ORSSerialPort) {
+        print("Serial port \(serialPort) was opened")
     }
     
 }
