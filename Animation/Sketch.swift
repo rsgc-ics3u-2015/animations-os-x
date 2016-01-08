c2455009848f23cf720ca1d98de97e364d24c66e
Russell Gordon <rgordon@rsgc.on.ca>
2016-01-08 11:18:28 -0500

    Using AudioKit classes to analyzing microphone input and control the position of the circle in the animation.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,15 +9,22 @@
 import Foundation
 
 class Sketch {
-
+    
     // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
     // Declare any properties you need for your sketch below this comment, but before init()
     var x = 0
+    var y = 0   // Vertical position of circle, will be tied to amplitude of mic input
     var s = 1
-
+    
+    // Objects needed to read mic input and analyze it
+    var analyzer: AKAudioAnalyzer!
+    let microphone = AKMicrophone()
+    let analysisSequence = AKSequence()
+    var updateAnalysis: AKEvent?
+    
     // This runs once, equivalent to setup() in Processing
     init() {
         
@@ -27,17 +34,18 @@ class Sketch {
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
         
-        // Play an audio tone (NOTE: First run, application may crash; seems to work on second run)
-        let instrument = AKInstrument()
-        instrument.setAudioOutput(AKOscillator())
-        AKOrchestra.addInstrument(instrument)
-        instrument.play()
+        // Initialize objects used to analyze mic input
+        analyzer = AKAudioAnalyzer(input: microphone.output)
+        AKOrchestra.addInstrument(microphone)
+        AKOrchestra.addInstrument(analyzer)
+        analyzer.play()
+        microphone.play()
         
     }
     
     // Runs repeatedly, equivalent to draw() in Processing
     func draw() {
-                
+        
         // Horizontal position of circle
         x = x + s
         
@@ -45,6 +53,12 @@ class Sketch {
         if (x > canvas.width || x < 0) {
             s *= -1
         }
+        
+        // Print amplitude of mic input (seems to vary between 0 and 1)
+        print("Amplitude as a float is: \(analyzer.trackedAmplitude.floatValue)")
+        
+        // Tie the vertical position of the circle to the amplitude
+        y = Int( Float(canvas.height) * analyzer.trackedAmplitude.floatValue )
         
         // "Clear" the background with a semi-transparent black rectangle
         canvas.drawShapesWithBorders = false
@@ -54,8 +68,8 @@ class Sketch {
         // Draw a circle that moves across the screen
         canvas.drawShapesWithBorders = false
         canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: x, centreY: canvas.height / 2, width: 25, height: 25)
-                
+        canvas.drawEllipse(centreX: x, centreY: y, width: 25, height: 25)
+        
     }
     
 }
