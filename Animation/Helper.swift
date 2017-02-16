d47a0a45b3412d1dd4ee5f70b24edeee9c4498c7
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-16 14:03:33 -0500

    Convenience function for random number generation.

--- Animation/Helper.swift
+++ Animation/Helper.swift
@@ -1,6 +1,21 @@
 import Cocoa
 import Foundation
 
+/**
+ Returns a random number in the given range.
+ 
+ - parameter from: The lowest possible random value that may be returned.
+ - parameter toButNotIncluding: The random number returned will never include this value.
+ 
+ */
+public func random(from : Int, toButNotIncluding : Int) -> Int {
+    
+    let max = UInt32(toButNotIncluding - from)
+    
+    return Int(arc4random_uniform(max)) + from
+    
+}
+
 open class Color {
     
     // FIXME: Need more research into how to properly write a class that handles invalid property geting/setting
@@ -400,6 +415,5 @@ open class Canvas : CustomPlaygroundQuickLookable {
         self.drawRectangle(bottomLeftX: centreX - width / 2, bottomLeftY: centreY - height / 2, width: width, height: height, borderWidth: borderWidth)
         
     }
-    
     
 }
