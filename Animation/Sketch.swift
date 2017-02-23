25d5c151b52dbb1738d4d3086206dcb16c71ea3e
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-23 12:44:54 -0500

    Changed the default animation.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -14,10 +14,25 @@ class Sketch : NSObject {
     //       Therefore, the line immediately below must always be present.
     let canvas : Canvas
     
-    // Declare any properties you need for your sketch below this comment, but before init()
+    // Declare constants
+    let north = 0
+    let northEast = 1
+    let east = 2
+    let southEast = 3
+    let south = 4
+    let southWest = 5
+    let west = 6
+    let northWest = 7
+    
+    // Set the step size (travel distance per iteration)
+    let stepSize = 5
+    
+    // Set the pixel size
+    let diameter = 7
+    
+    // Generate a starting position
     var x = 0
-    var s = 1
-    var backgroundRed : Bool = false // Boolean to toggle whether background is red
+    var y = 0
     
     // This runs once, equivalent to setup() in Processing
     override init() {
@@ -28,65 +43,68 @@ class Sketch : NSObject {
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
         
+        // Set starting position
+        x = random(from: 50, toButNotIncluding: canvas.width - 50)
+        y = random(from: 50, toButNotIncluding: canvas.height - 50)
+        
+        // No borders
+        canvas.drawShapesWithBorders = false
+        
     }
     
     // Runs repeatedly, equivalent to draw() in Processing
     func draw() {
         
-        // Horizontal position of circle
-        x = x + s
+        // Generate a random direction
+        var direction = random(from: 0, toButNotIncluding: 8)
         
-        // Bounce when hitting wall
-        if (x > canvas.width || x < 0) {
-            s *= -1
+        // Reverse direction if edge reached
+        if x + stepSize > canvas.width {
+            direction = west
+        }
+        if x - stepSize < 0 {
+            direction = east
+        }
+        if y + stepSize > canvas.height {
+            direction = south
+        }
+        if y - stepSize < 0 {
+            direction = north
         }
         
-        // "Clear" the background
-        canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 10)
-        // Decide what color to make the background
-        if (backgroundRed == true) {
-            canvas.fillColor = Color(hue: 0, saturation: 80, brightness: 90, alpha: 100)
-        } else {
-            canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 10)
+        // Change position based on direction and step size
+        if direction == north {
+            y = y + stepSize
+        } else if direction == northEast {
+            x = x + stepSize
+            y = y + stepSize
+        } else if direction == east {
+            x = x + stepSize
+        } else if direction == southEast {
+            x = x + stepSize
+            y = y - stepSize
+        } else if direction == south {
+            y = y - stepSize
+        } else if direction == southWest {
+            x = x - stepSize
+            y = y - stepSize
+        } else if direction == west {
+            x = x - stepSize
+        } else if direction == northWest {
+            x = x - stepSize
+            y = y + stepSize
         }
-        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: canvas.width, height: canvas.height)
         
-        
-        // Draw a circle that moves across the screen
-        canvas.drawShapesWithBorders = false
-        canvas.fillColor = Color(hue: Float(canvas.frameCount), saturation: 80, brightness: 90, alpha: 100)
-        canvas.drawEllipse(centreX: x, centreY: canvas.height / 2, width: 25, height: 25)
-        
-        // Draw some text on the screen
-        canvas.textColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100) // white
-        canvas.drawText(message: "Hello, world!")
-        
-        // Draw some more text on the screen
-        canvas.textColor = Color(hue: 60, saturation: 80, brightness: 90, alpha: 100) // yellow
-        canvas.drawText(message: "Current frame: \(canvas.frameCount)", size: 12, x: canvas.width / 16, y: canvas.height - canvas.height / 8)
-        
-        // Text with emoji works too
-        canvas.drawText(message: "😜", size: 48, x: canvas.width - canvas.width / 4, y: canvas.height - canvas.height / 4)
-        
-        // Draw a circle where the mouse pointer is
-        canvas.drawShapesWithBorders = false
-        canvas.drawShapesWithFill = true
-        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 100, alpha: 100)
-        canvas.drawEllipse(centreX: Int(canvas.mouseX), centreY: Int(canvas.mouseY), width: 5, height: 5)
+        // Draw the agent in it's new position
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 25)
+        canvas.drawEllipse(centreX: x, centreY: y, width: diameter, height: diameter)
         
     }
     
     // Respond to the mouseDown event
     func mouseDown() {
         
-        // When the mouse is pressed, change the background color that will be used.
-        if backgroundRed == true {
-            backgroundRed = false
-        } else {
-            backgroundRed = true
-        }
         
     }
-    
+
 }
